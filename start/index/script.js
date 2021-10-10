acdd175dc5743744f27256ddc3ff1013291c4d9e
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-10-10 13:29:26 +0530

    new

---
+++ start/index/script.js
@@ -0,0 +1,15 @@
+console.log('hello!');
+console.log(10 + 100);
+
+document.getElementById('demo2').innerHTML = 5 + 5 + 5;
+
+function myFunction() {
+  document.getElementById('demo3').innerHTML = 'paragraph changed';
+}
+
+function alert() {
+  window.alert(5 + 1);
+}
+
+document.write(1 + 1 * 2);
+alert(5 + 6);
