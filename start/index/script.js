013c3c872d4bbccd49d5bc49370ecf1f6a378d5a
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-10-18 10:42:09 +0530

    u

--- start/index/script.js
+++ start/index/script.js
@@ -13,3 +13,34 @@ function alert() {
 
 document.write(1 + 1 * 2);
 alert(5 + 6);
+
+function displayDate() {
+  document.getElementById('demo').innerHTML = Date();
+}
+
+let text = 'Double quotes "Quote" javascript';
+console.log(text.length);
+console.log(text);
+
+//objects
+
+let x = new String('JS');
+let y = new String('JS');
+
+if (x == y) console.log('yes');
+else console.log('no');
+
+let str = 'Microsoft, Binary tree, reverse';
+str = str.slice(1, 13);
+console.log(str);
+
+str = str.substr(7, 5);
+console.log(str);
+
+let text = 'microsoft microsofT';
+let newText = text.replace(/Microsoft/g, 'apple');
+console.log(newText);
+
+let sstr = 'abcdefghijklmnopqrstuvwxyz';
+sstr.lastIndexOf('x');
+console.log(sstr);
